4ca3a939ab2e369a4fd13f92b3f8736f1addd4ae
Keven Ferraz de Almeida <114549244+Kevenferraz39@users.noreply.github.com>
2024-01-15 16:09:25 -0300

    Atualização

---
+++ View/home.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--estilos-->
+    <link rel="stylesheet" href="style/home.css">
+    <!--estilos-->
+    <link rel="shortcut icon" href="./img/favicon.ico" type="image/x-icon">
+    <title>Power Play Palace | Ecommerce</title>
+</head>
+<body>
+    <div class="navbar">
+        <div class="header-inner-contet">
+            <a href="home.php"><h1 class="logo">Power <span>Play Palace</span></h1></a>
+            <a href="home.php" class="linkHome
+            Menu"><img src="./img/logo-removebg-preview.png" alt="logo" class="logoFoto"></a>
+            <nav>
+                <ul>
+                    <li><a href="home.php">HOME</a></li>
+                    <li><a href="produtos.php">PRODUTOS</a></li>
+                    <li><a href="sobre.php">SOBRE</a></li>
+                    <li><a href="contato.php">CONTATO</a></li>
+                    <li><a href="conta.php">CONTA</a></li>
+                </ul>
+            </nav>
+            <div class="nav-icons-container">
+                <img src="./img/carrinho.png" alt="carrinho" class="carrinhoimg" id="car-button">
+                <img src="./img/menuu.png" alt="menu" class="menu-button">
+            </div>
+        </div>
+    </div>
+    <div class="carrinho">
+        <h3>Carrinho:</h3>
+        <div class="compras">
+
+        </div>
+        <button>Ver mais &#8594;</button>
+    </div>
+    <header>
+        <div class="header-iner-content">
+            <div class="header-botton-side">
+                <div class="header-botton-side-left">
+                    <h2>De Um Novo Estilo ao Seu Trabalho<span>!</span></h2>
+                    <p>
+                        O sucesso nem sempre tem haver com grandeza. Tem haver com concistencia. Trabalho duro consistencia supera o sucesso. Agrandeza virá!
+                    </p>
+                    <button>Ver Agora &#8594;</button>
+                </div>
+                <div class="header-botton-side-right">
+                    <img src="./img/gaming-msi-header.png" alt="notebook">
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- main content -->
+
+    <main>
+        <div class="Orange-background">
+            <div class="page-inner-content">
+                <div class=" cols cols-3">
+                    <img src="./img/Products/product-1.png" alt="Produto 1">
+                    <img src="./img/Products/product-2.png" alt="Produto 2">
+                    <img src="./img/Products/product-3.png" alt="Produto 3"> 
+                </div>
+            </div>
+        </div>
+
+        <div>
+            <div class="page-inner-content">
+                <h3 class="section-title">Produtos selecionados</h3>
+                <div class="subtitle-underline"></div>
+            </div>
+            <div class="cols cols-4">
+                <div class="product">
+                    <img src="./img/Products/product-4.png" alt="Product 4">
+                    <p class="product-name">Combo Gamer Meetion</p>
+                    <p class="rate">&#9733;&#9733;&#9733;&#9734;&#9734;</p>
+                    <p class="product-price">4,000.00 <span>R$</span></p>
+                    <br>
+                    <button>Adicionar ao carrinho</button>
+
+                </div>
+
+                <div class="product">
+                    <img src="./img/Products/product-5.png" alt="Product 5">
+                    <p class="product-name">Placa de video GTX</p>
+                    <p class="rate">&#9733;&#9733;&#9733;&#9733;&#9733;</p>
+                    <p class="product-price">2,360.55 <span>R$</span></p>
+                    <br>
+                    <button>Adicionar ao carrinho</button>
+                </div>
+
+                <div class="product">
+                    <img src="./img/Products/product-6.png" alt="Product 6">
+                    <p class="product-name">Dualsense PS5</p>
+                    <p class="rate">&#9733;&#9733;&#9733;&#9733;&#9733;</p>
+                    <p class="product-price">500.00 <span>R$</span></p>
+                    <br>
+                    <button>Adicionar ao carrinho</button>
+                </div>
+
+                <div class="product">
+                    <img src="./img/Products/product-7.png" alt="Product 7">
+                    <p class="product-name">Cadeira Gamer </p>
+                    <p class="rate">&#9733;&#9733;&#9733;&#9734;&#9734;</p>
+                    <p class="product-price">450.99 <span>R$</span></p>
+                    <br>
+                    <button>Adicionar ao carrinho</button>
+                </div>
+                <!--Add product-->
+            </div>
+
+            <div class="page-inner-content">
+                <h3 class="section-title">
+                    Ultimos Produtos
+                </h3>
+                <div class="subtitle-underline"></div>
+            </div>
+            <div class="cols cols-4">
+                <div class="product">
+                    <img src="./img/Products/product-8.png" alt="Product 8">
+                    <p class="product-name">Iphone 14 Pro Max</p>
+                    <p class="rate">&#9733;&#9733;&#9733;&#9733;&#9733;</p>
+                    <p class="product-price">11,499.00 <span>R$</span></p>
+                    <br>
+                    <button>Adicionar ao carrinho</button>
+                </div>
+
+                <div class="product">
+                    <img src="./img/Products/product-9.png" alt="Product 9">
+                    <p class="product-name">Samsung Galaxy s23 Ultra</p>
+                    <p class="rate">&#9733;&#9733;&#9733;&#9733;&#9733;</p>
+                    <p class="product-price">6,299.55 <span>R$</span></p>
+                    <br>
+                    <button>Adicionar ao carrinho</button>
+                </div>
+
+                <div class="product">
+                    <img src="./img/Products/product-10.png" alt="Product 10">
+                    <p class="product-name">Asus Rog Phone 3 Gamer</p>
+                    <p class="rate">&#9733;&#9733;&#9733;&#9733;&#9733;</p>
+                    <p class="product-price">2,789.10 <span>R$</span></p>
+                    <br>
+                    <button>Adicionar ao carrinho</button>
+                </div>
+
+                <div class="product">
+                    <img src="./img/Products/product-11.png" alt="Product 11">
+                    <p class="product-name">Xaiomi Ultra 13 </p>
+                    <p class="rate">&#9733;&#9733;&#9733;&#9733;&#9734;</p>
+                    <p class="product-price">5,275.50 <span>R$</span></p>
+                    <br>
+                    <button>Adicionar ao carrinho</button>
+                </div>
+
+                <div class="product">
+                    <img src="./img/Products/product-12.png" alt="Product 12">
+                    <p class="product-name">Xbox Series X</p>
+                    <p class="rate">&#9733;&#9733;&#9733;&#9733;&#9733;</p>
+                    <p class="product-price">4,950.99 <span>R$</span></p>
+                    <br>
+                    <button>Adicionar ao carrinho</button>
+                </div>
+
+                <div class="product">
+                    <img src="./img/Products/product-13.png" alt="Product 13">
+                    <p class="product-name">Racingwheel GT480 Ferrari </p>
+                    <p class="rate">&#9733;&#9733;&#9733;&#9734;&#9734;</p>
+                    <p class="product-price">1,450.99 <span>R$</span></p>
+                    <br>
+                    <button>Adicionar ao carrinho</button>
+                </div>
+
+                <div class="product">
+                    <img src="./img/Products/product-14.png" alt="Product 14">
+                    <p class="product-name">Play Station 5 </p>
+                    <p class="rate">&#9733;&#9733;&#9733;&#9733;&#9734;</p>
+                    <p class="product-price">4,450.59 <span>R$</span></p>
+                    <br>
+                    <button>Adicionar ao carrinho</button>
+                </div>
+
+                <div class="product">
+                    <img src="./img/Products/product-15.png" alt="Product 15">
+                    <p class="product-name">Gamming dasktop RTX 3090 TI </p>
+                    <p class="rate">&#9733;&#9733;&#9733;&#9733;&#9734;</p>
+                    <p class="product-price">5,500.99 <span>R$</span></p>
+                    <br>
+                    <button>Adicionar ao carrinho</button>
+                </div>
+            </div>
+                 <div class="Orange-background">
+                    <div class="header-iner-content exclusive-container">
+                        <div class="header-botton-side">
+                            <div class="header-botton-side-left">
+                                <h2>Smart band 4</h2>
+                                <p>
+                                    O Mi smart band 4 tem uma tela AMOLED 39.9% maior que o Mi band 3, tela completament digital com brilho ajustavel, de tal modo que tudo é claro ao máximo
+                                </p>
+                                <button>Ver Agora &#8594;</button>
+                            </div>
+                            <div class="header-botton-side-right">
+                                <img src="./img/exclusive.png" alt="notebook">
+                            </div>
+                        </div>
+                    </div>
+                 </div>
+                <!--Add more product-->
+            </div>
+        </div>
+
+        <div>
+            <div class="page-inner-content">
+                <div class="feedback">
+                    <div class="feedbacks">
+                        <p>"</p>
+                        <p>A melhor loja online de vendas de eletronicos!</p>
+                        <p class="rate">&#9733;&#9733;&#9733;&#9733;&#9733;</p>
+                        <p>Usuario anonimo</p>
+                    </div>
+
+                    <div class="feedbacks">
+                        <p>"</p>
+                        <p>A melhor loja online de vendas de eletronicos!</p>
+                        <p class="rate">&#9733;&#9733;&#9733;&#9733;&#9733;</p>
+                        <p>Keven Ferraz</p>
+                    </div>
+
+                    <div class="feedbacks">
+                        <p>"</p>
+                        <p>A melhor loja online de vendas de eletronicos!</p>
+                        <p class="rate">&#9733;&#9733;&#9733;&#9733;&#9733;</p>
+                        <p>Kery Netrilyn</p>
+                    </div>
+                    <!--add more feedback`s-->
+                </div>
+            </div>
+        </div>
+    </main> 
+    <!-- main content -->
+    <footer class="Orange-background">
+        <div class="page-inner-content footer-content">
+            <div class="download-options">
+                <p>Baixe nosso apicativo</p>
+                <p>Baixe nosso apicativo para android & IOS</p>
+                <div>
+                    <img src="./img/app-store.png" alt="app-store">
+                    <img src="./img/play-store.png" alt="play-store">
+                </div>
+            </div>
+            <div class="logo-footer">
+                <img src="./img/logo3.png" alt="">
+                <p>De Um Novo Estilo ao Seu Trabalho<span>!</span></p>
+            </div>
+            <div class="links-footer">
+                <h3>Links úteis</h3>
+                <ul>
+                    <li>Cupons</li>
+                    <li>Blog Post</li>
+                    <li>Políticas de privacidade</li>
+                    <li>Torne-se afiliado</li>
+                </ul>
+            </div>
+        </div>
+        <hr class="page-inner-content">
+        <div class="page-inner-content copyright">
+            <p>Copyright 2024 Keven Ferraz - todos os direitos Reservados &copy;</p>
+        </div>
+    </footer>
+    <!--script-->
+    <script src="js/index.js"></script>
+    <script src="js/dropdown.js"></script>
+     <!--script-->
+</body>
+</html>
