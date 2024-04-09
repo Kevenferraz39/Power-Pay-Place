cdee8af03278ef1fc156e5344a33280eff20bc12
Keven Ferraz de Almeida <114549244+Kevenferraz39@users.noreply.github.com>
2024-04-09 10:54:36 +0000

    exclusao

--- Controller/sessao.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-	unset($_SESSION['UserID']);
-	unset($_SESSION['UserEmail']);
-	unset($_SESSION['UserEmail']);
-	unset($_SESSION);
-	session_destroy();
-	header("Location: ../View/index.php?sessao=1");
-?>
